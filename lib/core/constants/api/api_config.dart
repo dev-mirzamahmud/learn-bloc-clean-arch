a0e53f8b4fb597378962de5b3dc138660128a5d3
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-17 02:08:23 +0600

    add timeout constant config

--- lib/core/constants/api/api_config.dart
+++ lib/core/constants/api/api_config.dart
@@ -1,8 +1,10 @@
 class ApiConfig {
-  static const String SERVER_URL = 'https://api.escuelajs.co/';
-  static const String API_VERSION = 'api/v1/';
+  static const String _SERVER_URL = 'https://api.escuelajs.co/';
+  static const String _API_VERSION = 'api/v1/';
 
-  static const String BASE_URL = '$SERVER_URL$API_VERSION';
+  static const String BASE_URL = '$_SERVER_URL$_API_VERSION';
 
   static const Duration CONNECTION_TIME_OUT = Duration(seconds: 10);
+  static const Duration RECEIVE_TIME_OUT = Duration(seconds: 10);
+  static const Duration SEND_TIME_OUT = Duration(seconds: 10);
 }
